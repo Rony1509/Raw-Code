b8a73d27d599686ede6e8dad2e13f141019108a3
rony1509 <bsse1509@iit.du.ac.bd>
2026-04-23 02:11:16 +0600

    module

--- Node Js/Hello.js
+++ Node Js/Hello.js
@@ -1,5 +1,8 @@
 // app.js
 const math = require("./math");
+const strin = require("./strin");
 
+
+console.log(strin.atr("Hello,World!")); // Hello, World!
 console.log(math.square(4)); // 16
 console.log(math.cube(3));   // 27
---
+++ Node Js/strin.js
@@ -0,0 +1,5 @@
+const math = require("./math");
+function atr(str) {
+    console.log(str);
+}
+exports.atr = atr;
