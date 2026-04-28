c35a6deeb86a28dad20a247ff2e8bc4c3fcc9859
rony1509 <bsse1509@iit.du.ac.bd>
2026-04-29 01:55:13 +0600

    1.55

--- CP/Two_pointer.cpp
+++ CP/Two_pointer.cpp
@@ -2,4 +2,5 @@
 //Two pointer technique is used to solve problems related to arrays and linked lists. It involves using
 /*two pointers to traverse the data structure, often in opposite directions, to find a solution efficiently. This technique is commonly used for problems such as finding pairs with a given sum, removing duplicates from a sorted array, and reversing a linked list. By using two pointers, we can reduce the time complexity of certain algorithms from O(n^2) to O(n), making it an essential tool for competitive programming and coding interviews.*/
 /*Data is sorted or sorted can be easily done .then i can think two pointer
-Merged,Remove Duplicate,rearrangement,Detect cycle*/
+Merged,Remove Duplicate,rearrangement,Detect cycle
+pair,triplet,quadruple*/
